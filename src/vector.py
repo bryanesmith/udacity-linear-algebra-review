f898931a46181511a8da6c3449df89b31ba0049c
Bryan Smith <bryanesmith@gmail.com>
2018-12-31 17:37:39 -0500

    impl add, subtract, multi for vector

--- src/vector.py
+++ src/vector.py
@@ -1,3 +1,6 @@
+import numbers
+
+
 class Vector(object):
     def __init__(self, coordinates):
         try:
@@ -12,10 +15,41 @@ class Vector(object):
         except TypeError:
             raise TypeError('The coordinates must be an iterable')
 
-
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
 
-
     def __eq__(self, v):
         return self.coordinates == v.coordinates
+
+    # Constructs a new vector by iterating indices
+    def __indexmap(self, fn):
+        coords = []
+        for idx in range(0, self.dimension):
+            coords.append(fn(idx))
+
+        return Vector(coords)
+
+    # + overloading
+    def __add__(self, other):
+
+        if self.dimension != other.dimension:
+            raise ValueError("Dimensions of unequal length")
+
+        return self.__indexmap(lambda idx: self.coordinates[idx] + other.coordinates[idx])
+
+    # - overloading
+    def __sub__(self, other):
+
+        if self.dimension != other.dimension:
+            raise ValueError("Dimensions of unequal length")
+
+        return self.__indexmap(lambda idx: self.coordinates[idx] - other.coordinates[idx])
+
+    # * overloading
+    def __mul__(self, other):
+
+        if isinstance(other, numbers.Number):
+            return self.__indexmap(lambda idx: self.coordinates[idx] * other)
+
+        else:
+            raise TypeError("Unsupported type: {}".format(type(other)).__name__)
