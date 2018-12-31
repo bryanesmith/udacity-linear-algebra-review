37d3d8ad0589507f526d86c9ee46f5bbe1d808d9
Bryan Smith <bryanesmith@gmail.com>
2018-12-31 18:03:10 -0500

    list comprehensions ftw

--- src/vector.py
+++ src/vector.py
@@ -21,21 +21,13 @@ class Vector(object):
     def __eq__(self, v):
         return self.coordinates == v.coordinates
 
-    # Constructs a new vector by iterating indices
-    def __indexmap(self, fn):
-        coords = []
-        for idx in range(0, self.dimension):
-            coords.append(fn(idx))
-
-        return Vector(coords)
-
     # + overloading
     def __add__(self, other):
 
         if self.dimension != other.dimension:
             raise ValueError("Dimensions of unequal length")
 
-        return self.__indexmap(lambda idx: self.coordinates[idx] + other.coordinates[idx])
+        return Vector([x+y for x,y in zip(self.coordinates, other.coordinates)])
 
     # - overloading
     def __sub__(self, other):
@@ -43,13 +35,13 @@ class Vector(object):
         if self.dimension != other.dimension:
             raise ValueError("Dimensions of unequal length")
 
-        return self.__indexmap(lambda idx: self.coordinates[idx] - other.coordinates[idx])
+        return Vector([x-y for x,y in zip(self.coordinates, other.coordinates)])
 
     # * overloading
     def __mul__(self, other):
 
         if isinstance(other, numbers.Number):
-            return self.__indexmap(lambda idx: self.coordinates[idx] * other)
+            return Vector([x * other for x in self.coordinates])
 
         else:
             raise TypeError("Unsupported type: {}".format(type(other)).__name__)
