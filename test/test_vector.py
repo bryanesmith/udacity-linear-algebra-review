d63cef49faf7306af825613acc0b5ab17543f505
Bryan Smith <bryanesmith@gmail.com>
2019-01-05 13:52:08 -0500

    test cleanup

--- test/test_vector.py
+++ test/test_vector.py
@@ -4,7 +4,8 @@ from src.vector import Vector
 
 class TestVector(unittest.TestCase):
 
-    def __almostEqual(self, vec1, vec2):
+    # Helper method
+    def assertVectorsAlmostEqual(self, vec1, vec2):
         self.assertEqual(len(vec1), len(vec2))
         for idx in range(0, len(vec1)):
             self.assertAlmostEqual(vec1[idx], vec2[idx])
@@ -13,33 +14,31 @@ class TestVector(unittest.TestCase):
         v1 = Vector([8.218, -9.341])
         v2 = Vector([-1.129, 2.111])
         v3 = v1 + v2
-        self.__almostEqual(v3.coordinates, [7.089, -7.23])
+        self.assertVectorsAlmostEqual(v3.coordinates, [7.089, -7.23])
 
     def test_subtract(self):
         v1 = Vector([7.119, 8.215])
         v2 = Vector([-8.223, 0.878])
         v3 = v1 - v2
-        self.__almostEqual(v3.coordinates, [15.342, 7.337])
+        self.assertVectorsAlmostEqual(v3.coordinates, [15.342, 7.337])
 
     def test_multiply_scalar(self):
         v1 = Vector([1.671, -1.012, -0.318]) * 7.41
-        self.__almostEqual(v1.coordinates, [12.38211, -7.49892, -2.35638])
+        self.assertVectorsAlmostEqual(v1.coordinates, [12.38211, -7.49892, -2.35638])
 
-    def test_magnitude_1(self):
+    def test_magnitude(self):
         v1 = Vector([-0.221, 7.437])
         self.assertAlmostEquals(v1.magnitude(), 7.440282924728065)
 
-    def test_magnitude_2(self):
-        v1 = Vector([8.813, -1.331, -6.247])
-        self.assertAlmostEquals(v1.magnitude(), 10.884187567292289)
+        v2 = Vector([8.813, -1.331, -6.247])
+        self.assertAlmostEquals(v2.magnitude(), 10.884187567292289)
 
-    def test_normalize_1(self):
+    def test_normalize(self):
         v1 = Vector([5.581, -2.136])
-        self.__almostEqual(v1.normalize().coordinates, [0.9339352140866403, -0.35744232526233])
+        self.assertVectorsAlmostEqual(v1.normalize().coordinates, [0.9339352140866403, -0.35744232526233])
 
-    def test_normalize_2(self):
-        v1 = Vector([1.996, 3.108, -4.554])
-        self.__almostEqual(v1.normalize().coordinates, [0.3404012959433014, 0.5300437012984873, -0.7766470449528029])
+        v2 = Vector([1.996, 3.108, -4.554])
+        self.assertVectorsAlmostEqual(v2.normalize().coordinates, [0.3404012959433014, 0.5300437012984873, -0.7766470449528029])
 
     def test_dot_product(self):
         v1 = Vector([7.887, 4.138])
