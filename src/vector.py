47ecc6a27264243a0e728c9c3535e6eb95a58744
Bryan Smith <bryanesmith@gmail.com>
2019-01-05 14:17:34 -0500

    type annotations and corrections

--- src/vector.py
+++ src/vector.py
@@ -1,11 +1,11 @@
-import numbers
-from functools import reduce
-from math import sqrt, acos, degrees
 from decimal import Decimal
+from functools import reduce
+from math import acos, degrees
+from numbers import Number
 
 
 class Vector(object):
-    def __init__(self, coordinates):
+    def __init__(self, coordinates: [Number]):
         try:
             if not coordinates:
                 raise ValueError
@@ -18,26 +18,26 @@ class Vector(object):
         except TypeError:
             raise TypeError('The coordinates must be an iterable')
 
-    def __str__(self):
+    def __str__(self) -> str:
         return 'Vector: {}'.format(self.coordinates)
 
-    def __eq__(self, v):
+    def __eq__(self, v: 'Vector') -> bool:
         return self.coordinates == v.coordinates
 
     # + overloading
-    def __add__(self, other):
+    def __add__(self, other: 'Vector') -> 'Vector':
         self.__assert_equal_dimension(other)
-        return Vector([x+y for x,y in zip(self.coordinates, other.coordinates)])
+        return Vector([x+y for x, y in zip(self.coordinates, other.coordinates)])
 
     # - overloading
-    def __sub__(self, other):
+    def __sub__(self, other: 'Vector') -> 'Vector':
         self.__assert_equal_dimension(other)
-        return Vector([x-y for x,y in zip(self.coordinates, other.coordinates)])
+        return Vector([x-y for x, y in zip(self.coordinates, other.coordinates)])
 
     # * overloading
-    def __mul__(self, other):
+    def __mul__(self, other) -> 'Vector':
 
-        if isinstance(other, numbers.Number):
+        if isinstance(other, Number):
             return Vector([x * Decimal(other) for x in self.coordinates])
 
         else:
@@ -49,25 +49,26 @@ class Vector(object):
             raise ValueError("Dimensions of unequal length: {} vs {}".format(self.dimension, other.dimension))
 
     # magnitude of the vector
-    def magnitude(self):
-        total = reduce((lambda x,y: x + y ** 2), self.coordinates, 0)
-        return Decimal(sqrt(total))
+    def magnitude(self) -> Decimal:
+        total = reduce((lambda x, y: Decimal(x + y ** 2)), self.coordinates, 0)
+        return total.sqrt()
 
     # finds unit vector in same direction as this vector
-    def normalize(self):
+    def normalize(self) -> 'Vector':
         try:
             return self * (1 / self.magnitude())
         except ZeroDivisionError:
             raise Exception("Cannot normalize the zero vector")
 
     # finds dot product of two vectors
-    def dot_product(self, other: 'Vector'):
+    def dot_product(self, other: 'Vector') -> Decimal:
         self.__assert_equal_dimension(other)
         return sum([x*y for x, y in zip(self.coordinates, other.coordinates)])
 
-    # finds angle between two vectors
-    def angle_radians(self, other: 'Vector'):
-        return acos(self.dot_product(other) / (self.magnitude() * other.magnitude()))
+    # finds angle in radians between two vectors
+    def angle_radians(self, other: 'Vector') -> Decimal:
+        return Decimal(acos(self.dot_product(other) / (self.magnitude() * other.magnitude())))
 
-    def angle_degrees(self, other: 'Vector'):
-        return degrees(self.angle_radians(other))
+    # finds angle in degrees between two vectors
+    def angle_degrees(self, other: 'Vector') -> Decimal:
+        return Decimal(degrees(self.angle_radians(other)))
