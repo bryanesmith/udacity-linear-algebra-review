27dc4728ae0b16a9e32fb4964761c0214e9751c1
Bryan Smith <bryanesmith@gmail.com>
2019-01-05 13:47:46 -0500

    Vector.dot_product, Vector.angle_radians, Vector.angle_degress

--- src/program.py
+++ src/program.py
@@ -2,18 +2,21 @@ from src.vector import Vector
 
 
 def main():
-    v1 = Vector([-0.221, 7.437])
-    print(v1.magnitude())
+    v1 = Vector([7.887, 4.138])
+    v2 = Vector([-8.802, 6.776])
+    print(v1.dot_product(v2))
 
-    v2 = Vector([8.813, -1.331, -6.247])
-    print(v2.magnitude())
+    v3 = Vector([-5.955, -4.904, -1.874])
+    v4 = Vector([-4.496, -8.755, 7.103])
+    print(v3.dot_product(v4))
 
-    v3 = Vector([5.581, -2.136])
-    print(v3.normalize())
+    v5 = Vector([3.183, -7.627])
+    v6 = Vector([-2.668, 5.319])
+    print(v5.angle_radians(v6))
 
-    v4 = Vector([1.996, 3.108, -4.554])
-    print(v4.normalize())
-
+    v7 = Vector([7.35, 0.221, 5.188])
+    v8 = Vector([2.751, 8.259, 3.985])
+    print(v7.angle_degrees(v8))
 
 if __name__ == '__main__':
     main()
--- src/vector.py
+++ src/vector.py
@@ -1,6 +1,6 @@
 import numbers
 from functools import reduce
-from math import sqrt
+from math import sqrt, acos, degrees
 
 
 class Vector(object):
@@ -25,18 +25,12 @@ class Vector(object):
 
     # + overloading
     def __add__(self, other):
-
-        if self.dimension != other.dimension:
-            raise ValueError("Dimensions of unequal length")
-
+        self.__assert_equal_dimension(other)
         return Vector([x+y for x,y in zip(self.coordinates, other.coordinates)])
 
     # - overloading
     def __sub__(self, other):
-
-        if self.dimension != other.dimension:
-            raise ValueError("Dimensions of unequal length")
-
+        self.__assert_equal_dimension(other)
         return Vector([x-y for x,y in zip(self.coordinates, other.coordinates)])
 
     # * overloading
@@ -47,6 +41,11 @@ class Vector(object):
 
         else:
             raise TypeError("Unsupported type: {}".format(type(other)).__name__)
+
+    # private: check for equal dimensions
+    def __assert_equal_dimension(self, other: 'Vector'):
+        if self.dimension != other.dimension:
+            raise ValueError("Dimensions of unequal length: {} vs {}".format(self.dimension, other.dimension))
 
     # magnitude of the vector
     def magnitude(self):
@@ -59,3 +58,15 @@ class Vector(object):
             return self * (1 / self.magnitude())
         except ZeroDivisionError:
             raise Exception("Cannot normalize the zero vector")
+
+    # finds dot product of two vectors
+    def dot_product(self, other: 'Vector'):
+        self.__assert_equal_dimension(other)
+        return sum([x*y for x, y in zip(self.coordinates, other.coordinates)])
+
+    # finds angle between two vectors
+    def angle_radians(self, other: 'Vector'):
+        return acos(self.dot_product(other) / (self.magnitude() * other.magnitude()))
+
+    def angle_degrees(self, other: 'Vector'):
+        return degrees(self.angle_radians(other))
